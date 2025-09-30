bb627c6dd6287c6b0ce2d26a9871b81591acec5d
CLFlix <remcoslootmaekers@gmail.com>
2025-09-30 21:47:07 +0200

    simplified get_twitch_refresh_token

--- get_twitch_refresh_token.py
+++ get_twitch_refresh_token.py
@@ -8,18 +8,17 @@ load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 CODE = os.getenv("CODE")
-REDIRECT_URI = "http://localhost"
 
 def get_refresh_token():
-    response = requests.post("https://id.twitch.tv/oauth2/token",
-                            data={
-                                "client_id": CLIENT_ID,
-                                "client_secret": CLIENT_SECRET,
-                                "code": CODE,
-                                "grant_type": "authorization_code",
-                                "redirect_uri": REDIRECT_URI
-                            }
-                        )
+    uri = "https://id.twitch.tv/oauth2/token"
+    params = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "code": CODE,
+        "grant_type": "authorization_code",
+        "redirect_uri": "http:localhost"
+    }
+    response = requests.post(uri, params=params)
 
     if response.status_code == 401:
         raise ConnectionError("Double-check your credentials and try again. You might have to re-run 'get_twitch_refresh_token.py'. In that case, you don't have to run this one again.")
