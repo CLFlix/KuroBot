8f8355ab2a3e1af08b5432c42dd448f125963005
CLFlix <remcoslootmaekers@gmail.com>
2025-10-10 23:34:19 +0200

    clarified wip pages

--- website/app/about/page.tsx
+++ website/app/about/page.tsx
@@ -1,14 +1,23 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 function About() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="text-center mt-2">
-        <h1 className="text-2xl text-center">About</h1>
-        <p>Work in progress...</p>
+      <main className="flex flex-col items-center justify-center flex-1">
+        <Image
+          src="/wip.png"
+          width={200}
+          height={200}
+          alt="Attention board"
+        />
+        <section className="text-center mt-4">
+          <h1 className="text-3xl">About</h1>
+          <p>Work in progress...</p>
+        </section>
       </main>
-    </>
+    </div>
   );
 }
 
