14c524845724d1aca70be7961f2efbb506003990
CLFlix <remcoslootmaekers@gmail.com>
2025-10-10 23:43:27 +0200

    styled header

--- website/app/commands/page.tsx
+++ website/app/commands/page.tsx
@@ -5,8 +5,7 @@ async function Commands() {
   return (
     <>
       <Header />
-      <main className="ml-auto">
-        <h1 className="text-2xl text-center">Commands</h1>
+      <main className="mt-3">
         <section className="grid grid-cols-2">
           <CommandsClientView />
         </section>
--- website/components/Header.tsx
+++ website/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <ul className="bg-black flex flex-row gap-5 text-2xl font-sans p-px justify-center list-none">
+        <ul className="bg-purple-600 text-white text-3xl font-sans flex justify-center gap-6">
           <Link href="/">Home</Link>
           <Link href="/commands">Commands</Link>
           <Link href="/about">About</Link>
