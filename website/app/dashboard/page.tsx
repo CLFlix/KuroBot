302e72a00a7c0e2dee9d03c09ebbe8a35321a687
CLFlix <remcoslootmaekers@gmail.com>
2025-12-26 00:10:19 +0100

    added more info to the dashboard

--- website/app/dashboard/page.tsx
+++ website/app/dashboard/page.tsx
@@ -4,6 +4,9 @@ import { StatusMessage } from "@/types";
 import { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [takeRequests, setTakeRequests] = useState<boolean>(false);
+  const [titleUpdaterOn, setTitleUpdaterOn] = useState<boolean>(false);
   const [rank, setRank] = useState<number | null>(null);
   const [title, setTitle] = useState<string>("");
   const [points, setPoints] = useState<Record<string, number>>();
@@ -11,7 +14,21 @@ const Dashboard = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [statusMessages, setStatusMessages] = useState<StatusMessage[]>([]);
 
+  const getTitleUpdaterStatus = async () => {
+    const res = await fetch("http://localhost:7273/titleUpdaterOn");
+
+    if (!res.ok) {
+      setTitleUpdaterOn(false);
+      return;
+    }
+
+    const data = await res.json();
+    setTitleUpdaterOn(data);
+  };
+
   const getTitle = async () => {
+    if (!isRunning) return;
+
     const res = await fetch("http://localhost:7273/twitchTitle");
 
     if (!res.ok) {
@@ -29,6 +46,8 @@ const Dashboard = () => {
   };
 
   const getRank = async () => {
+    if (!isRunning) return;
+
     const res = await fetch("http://localhost:7273/rank");
 
     if (!res.ok) throw new Error("Could not get current osu! rank.");
@@ -45,6 +64,8 @@ const Dashboard = () => {
   };
 
   const getPoints = async () => {
+    if (!isRunning) return;
+
     const res = await fetch("http://localhost:7273/points");
 
     if (!res.ok) throw new Error("Could not get points.");
@@ -54,25 +75,66 @@ const Dashboard = () => {
     sortPoints(data);
   };
 
+  const getTakesRequests = async () => {
+    if (!isRunning) return;
+
+    const res = await fetch("http://localhost:7273/takeRequests");
+
+    if (!res.ok) throw new Error("Couldn't get requests status");
+
+    const data = await res.json();
+    setTakeRequests(data === true);
+  };
+
+  useEffect(() => {
+    getTakesRequests();
+    getTitleUpdaterStatus();
+  }, [isRunning]);
+
   useEffect(() => {
     getRank();
     getTitle();
-    getPoints();
 
-    const current_rank = setInterval(getRank, 60000); // change to something lower for dev
-    const current_title = setInterval(getTitle, 60000); // change to something lower for dev
-    const bot_points = setInterval(getPoints, 10000); // change to something lower for dev
+    const current_rank = setInterval(getRank, 5000); // change to something lower for dev
+    const current_title = setInterval(getTitle, 5000);
 
     return () => {
       clearInterval(current_rank);
       clearInterval(current_title);
-      clearInterval(bot_points);
     };
-  }, []);
+  }, [isRunning]);
+
+  useEffect(() => {
+    const getIsRunning = async () => {
+      await fetch("http://localhost:7273/isRunning")
+        .then((data) => {
+          data.json().then((hello) => setIsRunning(hello === "hello"));
+        })
+        .catch((err) => setIsRunning(false));
+    };
+    getIsRunning();
+
+    const interval = setInterval(getIsRunning, 5000);
+    return () => clearInterval(interval);
+  });
+
+  useEffect(() => {
+    getPoints();
+
+    const bot_points = setInterval(getPoints, 10000); // change to something lower for dev
+
+    return () => clearInterval(bot_points);
+  }, [isRunning]);
 
   const updateTitle = async () => {
     setLoading(true);
     setStatusMessages([]);
+
+    if (!isRunning) {
+      setStatusMessages([{ message: "Bot is not running...", type: "error" }]);
+      return;
+    }
+
     const response = await fetch("http://localhost:7273/update_title");
 
     if (!response.ok) {
@@ -105,69 +167,116 @@ const Dashboard = () => {
   }, [statusMessages]);
 
   return (
-    <main className="mt-4 ml-4">
-      <h1 className="text-3xl font-bold">KuroBot Dashboard</h1>
-      <table className="mt-2">
-        <thead>
-          <tr className="text-2xl">
-            <td className="table-header-gradient px-3">osu! Rank</td>
-            <td className="table-header-gradient px-3">Current Stream Title</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="text-center">
-            <td className="table-command-gradient">{rank}</td>
-            <td className="table-command-gradient">{title}</td>
-          </tr>
-        </tbody>
-      </table>
+    <main className="mt-4 mx-4">
+      <h1 className="text-3xl font-bold text-center">KuroBot Dashboard</h1>
+      <div className="grid grid-cols-3">
+        <div>
+          {isRunning && (
+            <>
+              <h2 className="text-2xl font-bold mt-2">Title Updates</h2>
+              <table className="mt-2">
+                <thead>
+                  <tr className="text-2xl">
+                    <td className="table-header-gradient px-3">osu! Rank</td>
+                    <td className="table-header-gradient px-3">
+                      Current Stream Title
+                    </td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="text-center">
+                    <td className="table-command-gradient">{rank}</td>
+                    <td className="table-command-gradient">{title}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="flex items-center gap-3">
+                <button
+                  onClick={() => updateTitle()}
+                  className="mt-3 discord-button"
+                  disabled={loading}
+                >
+                  Update Stream Title
+                </button>
+                {statusMessages.length > 0 && (
+                  <p
+                    className={
+                      statusMessages[0].type === "success"
+                        ? "text-green-400 mt-3"
+                        : "text-red-500 mt-3"
+                    }
+                  >
+                    {statusMessages[0].message}
+                  </p>
+                )}
+              </div>
+              <div className="mt-4">
+                <p className="text-xl">
+                  <span className="font-bold">Automatic title updater:</span>{" "}
+                  {titleUpdaterOn ? "On" : "Off"}
+                </p>
+              </div>
+            </>
+          )}
+        </div>
 
-      <div className="flex items-center gap-3">
-        <button
-          onClick={() => updateTitle()}
-          className="mt-3 discord-button"
-          disabled={loading}
-        >
-          Update Stream Title
-        </button>
-        {statusMessages.length > 0 && (
-          <p
-            className={
-              statusMessages[0].type === "success"
-                ? "text-green-400 mt-3"
-                : "text-red-500 mt-3"
-            }
-          >
-            {statusMessages[0].message}
-          </p>
+        <div className="flex flex-col text-center">
+          <div>
+            <h2 className="text-2xl font-bold mt-2">Bot status</h2>
+            {isRunning ? (
+              <div>
+                <p className="text-xl text-green-400">Running...</p>
+              </div>
+            ) : (
+              <div>
+                <p className="text-xl text-red-500">Not running...</p>
+              </div>
+            )}
+          </div>
+          {isRunning && (
+            <>
+              <h2 className="text-2xl font-bold mt-4">Taking requests?</h2>
+              <p
+                className={`text-xl ${
+                  takeRequests ? "text-green-400" : "text-red-400"
+                }`}
+              >
+                {takeRequests ? "Yes" : "No"}
+              </p>
+            </>
+          )}
+        </div>
+
+        {isRunning && (
+          <div>
+            {points && (
+              <div>
+                <h2 className="text-xl font-bold mt-2">Top 5 Points Owners:</h2>
+                <table className="mt-3">
+                  <thead>
+                    <tr className="text-center text-2xl">
+                      <td className="table-header-gradient px-3">Username</td>
+                      <td className="table-header-gradient px-3">Amount</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {top5.map((user, index) => (
+                      <tr key={index}>
+                        <td className="px-2 py-1 table-command-gradient">
+                          {user[0]}
+                        </td>
+                        <td className="px-2 py-1 table-command-gradient text-center">
+                          {user[1]}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
         )}
       </div>
-
-      {points && (
-        <div>
-          <h2 className="text-xl font-bold mt-5">Top 5 Points Owners:</h2>
-          <table className="mt-3">
-            <thead>
-              <tr className="text-center text-2xl">
-                <td className="table-header-gradient px-3">Username</td>
-                <td className="table-header-gradient px-3">Amount</td>
-              </tr>
-            </thead>
-            <tbody>
-              {top5.map((user, index) => (
-                <tr key={index}>
-                  <td className="px-2 py-1 table-command-gradient">
-                    {user[0]}
-                  </td>
-                  <td className="px-2 py-1 table-command-gradient text-center">
-                    {user[1]}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
     </main>
   );
 };
