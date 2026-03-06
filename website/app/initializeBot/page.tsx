3acae8723fb4e6ea7ef2f90f06b24dc654798ffc
CLFlix <remcoslootmaekers@gmail.com>
2026-03-07 00:20:42 +0100

    check for initialized status
    otherwise users can keep initializing without anything happening

--- website/app/initializeBot/page.tsx
+++ website/app/initializeBot/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const initializeBot = () => {
   const router = useRouter();
@@ -9,6 +10,22 @@ const initializeBot = () => {
   const [rq, setRq] = useState<boolean>(false);
   const [affiliate, setAffiliate] = useState<boolean>(false);
   const [update, setUpdate] = useState<boolean>(false);
+  const [initStatus, setInitStatus] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (initStatus) return;
+
+    const getInitializedStatus = async () => {
+      await fetch("/initStatus").then((data) =>
+        data
+          .json()
+          .then((status) => setInitStatus(status))
+          .catch((err) => console.error(err)),
+      );
+    };
+
+    getInitializedStatus();
+  }, [initStatus]);
 
   const startupBot = async (
     rq: boolean,
@@ -22,61 +39,71 @@ const initializeBot = () => {
     });
     if (!res.ok) throw new Error("Something went wrong starting up the bot...");
 
-    router.push("/dashboard");
+    router.push("http://localhost:7273/");
   };
 
   return (
     <main className="m-4 flex justify-center">
       <div>
-        <form className="text-lg">
-          <div>
-            <label htmlFor="rq_selection">
-              Do you want to take map requests during the stream?
-            </label>
-            <select
-              name="rq_selection"
-              onChange={(e) => setRq(e.target.value === "requests_yes")}
-            >
-              <option value="requests_no">No</option>
-              <option value="requests_yes">Yes</option>
-            </select>
+        {initStatus ? (
+          <div className="text-xl">
+            <Link href="http://localhost:7273/" className="link">
+              Go to the dashboard..
+            </Link>
           </div>
-          <div>
-            <label htmlFor="affiliate_selection">
-              Are you a Twitch Affiliate / Partner?
-            </label>
-            <select
-              name="affiliate_selection"
-              onChange={(e) => setAffiliate(e.target.value === "affiliate_yes")}
-            >
-              <option value="affiliate_no">No</option>
-              <option value="affiliate_yes">Yes</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="update_selection">
-              Do you want your rank to be automatically updated in your title?
-            </label>
-            <select
-              name="update_selection"
-              onChange={(e) => setUpdate(e.target.value === "update_yes")}
-            >
-              <option value="update_no">No</option>
-              <option value="update_yes">Yes</option>
-            </select>
-          </div>
-          <div className="flex justify-center">
-            <button
-              className="discord-button mt-2"
-              onClick={(e) => {
-                e.preventDefault();
-                startupBot(rq, affiliate, update);
-              }}
-            >
-              Start bot
-            </button>
-          </div>
-        </form>
+        ) : (
+          <form className="text-lg">
+            <div>
+              <label htmlFor="rq_selection">
+                Do you want to take map requests during the stream?
+              </label>
+              <select
+                name="rq_selection"
+                onChange={(e) => setRq(e.target.value === "requests_yes")}
+              >
+                <option value="requests_no">No</option>
+                <option value="requests_yes">Yes</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="affiliate_selection">
+                Are you a Twitch Affiliate / Partner?
+              </label>
+              <select
+                name="affiliate_selection"
+                onChange={(e) =>
+                  setAffiliate(e.target.value === "affiliate_yes")
+                }
+              >
+                <option value="affiliate_no">No</option>
+                <option value="affiliate_yes">Yes</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="update_selection">
+                Do you want your rank to be automatically updated in your title?
+              </label>
+              <select
+                name="update_selection"
+                onChange={(e) => setUpdate(e.target.value === "update_yes")}
+              >
+                <option value="update_no">No</option>
+                <option value="update_yes">Yes</option>
+              </select>
+            </div>
+            <div className="flex justify-center">
+              <button
+                className="discord-button mt-2"
+                onClick={(e) => {
+                  e.preventDefault();
+                  startupBot(rq, affiliate, update);
+                }}
+              >
+                Start bot
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </main>
   );
