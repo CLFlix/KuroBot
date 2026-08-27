97d4f9328a671ad3852e487996daea7918324663
CLFlix <remcoslootmaekers@gmail.com>
2026-08-27 14:18:10 +0200

    stop requesting original vips
    there was no real reason for doing this

--- bot/bot.py
+++ bot/bot.py
@@ -332,7 +332,6 @@ class KuroBot(commands.Bot):
         self.user_points[self.user_id] = float("inf")
         self.check_for_update()
         self.get_mods_list()
-        self.get_vips_list()
         # self.export_commands() # ONLY USED FOR UPDATING WEBSITE COMMANDS
 
         while not self.initialized:
@@ -430,9 +429,6 @@ class KuroBot(commands.Bot):
 
     def get_follower_data(self, user_id):
         return self.api.get_follower_data(user_id)
-    
-    def get_vips_list(self):
-        return self.api.get_vip_list()
 
     def add_vip(self, user_id):
         return self.api.add_vip(user_id)
